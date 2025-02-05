859a9ba0b72535f47d4ab6b37176e7975f809fd7
Alexis Urien <alexis.urien@free.fr>
2025-02-05 11:32:49 -0800

    Add millions minutes

---
+++ src/Recipe/MillionMinutes.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Axi\MyCalendar\Recipe;
+
+use Axi\MyCalendar\Event;
+use Symfony\Component\Translation\TranslatableMessage;
+
+class MillionMinutes extends Recipe
+{
+    private const ONE_MILLION = 10000000;
+
+    public function getEvents(\DateTimeImmutable $basedOn): array
+    {
+        $events = [];
+        $maxYearsinMinutes = $this->maxYear * self::EARTH_DAYS_PER_YEAR * 24 * 60;
+
+        $i = 1;
+        while ($i * self::ONE_MILLION < $maxYearsinMinutes) {
+            $nbMinutes = $i * self::ONE_MILLION;
+            $event = new Event(
+                (clone $basedOn)->add(new \DateInterval('PT' . $nbMinutes . 'M'))
+            );
+            $event->setSummary($this->getSummary($nbMinutes));
+            $event->setSourceRecipe(self::class);
+            $events[] = $event;
+            $i++;
+        }
+
+        return $events;
+    }
+
+    public function getSummary(...$vars): TranslatableMessage
+    {
+        return new TranslatableMessage(
+            'recipe.minutes',
+            [
+                'nbMinutes' => $vars[0],
+            ]
+        );
+    }
+
+    public function getSource(): string
+    {
+        return '';
+    }
+}
